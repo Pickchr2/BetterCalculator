f19d91401f5b76793cf2f3bd94798ace227795e1
Chris Pickens <Cpickens1270@gmail.com>
2023-01-27 23:15:32 -0700

    Add project files.

---
+++ BetterCalculator/BetterCalculator.vb
@@ -0,0 +1,17 @@
+'Christopher Pickens
+'RCET0265
+'Spring 2023
+'Better Calculator
+'https://github.com/Pickchr/BetterCalculator.Git
+
+Option Explicit On
+Option Strict On
+Option Compare Text
+
+Imports System
+
+Module BetterCalculator
+    Sub Main(args As String())
+
+    End Sub
+End Module
